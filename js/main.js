67d5559749b1ca8a3d6d0a68ccd33211e6c782de
Carlinda Lee <51838006+carlindalee@users.noreply.github.com>
2021-07-18 02:03:48 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -35,29 +35,7 @@
 		}
 
 	};
-// Logos
-	$(document).ready(function() {
-  $('.logo-carousel').slick({
-    slidesToShow: 6,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 1000,
-    arrows: true,
-    dots: false,
-    pauseOnHover: false,
-    responsive: [{
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 4
-      }
-    }, {
-      breakpoint: 520,
-      settings: {
-        slidesToShow: 2
-      }
-    }]
-  });
-});
+
 	// Animations
 
 	var contentWayPoint = function() {
